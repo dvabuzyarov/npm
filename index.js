35b6613eead17b426e27d2e3e7cf39015d4dcf4e
dvabuzyarov <dvabuzyarov@gmail.com>
2020-03-27 12:37:29 +0200

    feature: support for monorepo has been introduced

--- index.js
+++ index.js
@@ -14,109 +14,113 @@ let prepared;
 const npmrc = tempy.file({name: '.npmrc'});
 
 async function verifyConditions(pluginConfig, context) {
-  // If the npm publish plugin is used and has `npmPublish`, `tarballDir` or `pkgRoot` configured, validate them now in order to prevent any release if the configuration is wrong
-  if (context.options.publish) {
-    const publishPlugin =
-      castArray(context.options.publish).find(config => config.path && config.path === '@semantic-release/npm') || {};
+    // If the npm publish plugin is used and has `npmPublish`, `tarballDir` or `pkgRoot` configured, validate them now in order to prevent any release if the configuration is wrong
+    if (context.options.publish) {
+        const publishPlugin =
+            castArray(context.options.publish).find(config => config.path && config.path === '@semantic-release/npm') || {};
 
-    pluginConfig.npmPublish = defaultTo(pluginConfig.npmPublish, publishPlugin.npmPublish);
-    pluginConfig.tarballDir = defaultTo(pluginConfig.tarballDir, publishPlugin.tarballDir);
-    pluginConfig.pkgRoot = defaultTo(pluginConfig.pkgRoot, publishPlugin.pkgRoot);
-  }
-
-  const errors = verifyNpmConfig(pluginConfig);
-
-  setLegacyToken(context);
-
-  try {
-    const pkg = await getPkg(pluginConfig, context);
-
-    // Verify the npm authentication only if `npmPublish` is not false and `pkg.private` is not `true`
-    if (pluginConfig.npmPublish !== false && pkg.private !== true) {
-      await verifyNpmAuth(npmrc, pkg, context);
+        pluginConfig.npmPublish = defaultTo(pluginConfig.npmPublish, publishPlugin.npmPublish);
+        pluginConfig.tarballDir = defaultTo(pluginConfig.tarballDir, publishPlugin.tarballDir);
+        pluginConfig.pkgRoot = defaultTo(pluginConfig.pkgRoot, publishPlugin.pkgRoot);
     }
-  } catch (error) {
-    errors.push(...error);
-  }
 
-  if (errors.length > 0) {
-    throw new AggregateError(errors);
-  }
+    const errors = verifyNpmConfig(pluginConfig);
 
-  verified = true;
+    setLegacyToken(context);
+
+    for (const pkgRoot of pluginConfig.pkgRoot) {
+        try {
+
+            const pkg = await getPkg({pkgRoot}, context);
+
+            // Verify the npm authentication only if `npmPublish` is not false and `pkg.private` is not `true`
+            if (pluginConfig.npmPublish !== false && pkg.private !== true) {
+                await verifyNpmAuth(npmrc, pkg, context);
+            }
+        } catch (error) {
+            errors.push(...error);
+        }
+    }
+
+    if (errors.length > 0) {
+        throw new AggregateError(errors);
+    }
+
+    verified = true;
 }
 
 async function prepare(pluginConfig, context) {
-  const errors = verified ? [] : verifyNpmConfig(pluginConfig);
+    const errors = verified ? [] : verifyNpmConfig(pluginConfig);
 
-  setLegacyToken(context);
-
-  try {
-    // Reload package.json in case a previous external step updated it
-    const pkg = await getPkg(pluginConfig, context);
-    if (!verified && pluginConfig.npmPublish !== false && pkg.private !== true) {
-      await verifyNpmAuth(npmrc, pkg, context);
+    setLegacyToken(context);
+    for (const pkgRoot of pluginConfig.pkgRoot) {
+        try {
+            // Reload package.json in case a previous external step updated it
+            const pkg = await getPkg({pkgRoot}, context);
+            if (!verified && pluginConfig.npmPublish !== false && pkg.private !== true) {
+                await verifyNpmAuth(npmrc, pkg, context);
+            }
+        } catch (error) {
+            errors.push(...error);
+        }
     }
-  } catch (error) {
-    errors.push(...error);
-  }
+    if (errors.length > 0) {
+        throw new AggregateError(errors);
+    }
 
-  if (errors.length > 0) {
-    throw new AggregateError(errors);
-  }
-
-  await prepareNpm(npmrc, pluginConfig, context);
-  prepared = true;
+    await prepareNpm(npmrc, pluginConfig, context);
+    prepared = true;
 }
 
 async function publish(pluginConfig, context) {
-  let pkg;
-  const errors = verified ? [] : verifyNpmConfig(pluginConfig);
+    let pkg;
+    const errors = verified ? [] : verifyNpmConfig(pluginConfig);
 
-  setLegacyToken(context);
-
-  try {
-    // Reload package.json in case a previous external step updated it
-    pkg = await getPkg(pluginConfig, context);
-    if (!verified && pluginConfig.npmPublish !== false && pkg.private !== true) {
-      await verifyNpmAuth(npmrc, pkg, context);
+    setLegacyToken(context);
+    for (const pkgRoot of pluginConfig.pkgRoot) {
+        try {
+            // Reload package.json in case a previous external step updated it
+            pkg = await getPkg({pkgRoot}, context);
+            if (!verified && pluginConfig.npmPublish !== false && pkg.private !== true) {
+                await verifyNpmAuth(npmrc, pkg, context);
+            }
+        } catch (error) {
+            errors.push(...error);
+        }
     }
-  } catch (error) {
-    errors.push(...error);
-  }
+    if (errors.length > 0) {
+        throw new AggregateError(errors);
+    }
 
-  if (errors.length > 0) {
-    throw new AggregateError(errors);
-  }
+    if (!prepared) {
+        await prepareNpm(npmrc, pluginConfig, context);
+    }
 
-  if (!prepared) {
-    await prepareNpm(npmrc, pluginConfig, context);
-  }
-
-  return publishNpm(npmrc, pluginConfig, pkg, context);
+    return publishNpm(npmrc, pluginConfig, pkg, context);
 }
 
 async function addChannel(pluginConfig, context) {
-  let pkg;
-  const errors = verified ? [] : verifyNpmConfig(pluginConfig);
+    let pkg;
+    const errors = verified ? [] : verifyNpmConfig(pluginConfig);
 
-  setLegacyToken(context);
-
-  try {
-    // Reload package.json in case a previous external step updated it
-    pkg = await getPkg(pluginConfig, context);
-    if (!verified && pluginConfig.npmPublish !== false && pkg.private !== true) {
-      await verifyNpmAuth(npmrc, pkg, context);
+    setLegacyToken(context);
+    for (const pkgRoot of pluginConfig.pkgRoot) {
+        try {
+            // Reload package.json in case a previous external step updated it
+            pkg = await getPkg({pkgRoot}, context);
+            if (!verified && pluginConfig.npmPublish !== false && pkg.private !== true) {
+                await verifyNpmAuth(npmrc, pkg, context);
+            }
+        } catch (error) {
+            errors.push(...error);
+        }
     }
-  } catch (error) {
-    errors.push(...error);
-  }
 
-  if (errors.length > 0) {
-    throw new AggregateError(errors);
-  }
+    if (errors.length > 0) {
+        throw new AggregateError(errors);
+    }
 
-  return addChannelNpm(npmrc, pluginConfig, pkg, context);
+    return addChannelNpm(npmrc, pluginConfig, pkg, context);
 }
 
 module.exports = {verifyConditions, prepare, publish, addChannel};
